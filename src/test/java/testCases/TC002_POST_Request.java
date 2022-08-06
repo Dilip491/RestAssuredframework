fb48f041b8fbe3e1c0937d1e39ffd7fcefdf81d1
Dilip491 <dilip9418@gmail.com>
2022-08-06 15:06:22 +0530

    Modified TC002_POST_Request.java

--- src/test/java/testCases/TC002_POST_Request.java
+++ src/test/java/testCases/TC002_POST_Request.java
@@ -3,6 +3,7 @@ package testCases;
 public class TC002_POST_Request {
 
     public static void main(String[] args) {
-        System.out.println("New test case has been added");
+        System.out.println("New test case added");
+        System.out.println("The test case has been modified");
     }
 }
